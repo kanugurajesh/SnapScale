d83c1d2e88a3502991de6c8f3bcb7164c5280689
Rajesh604 <kanugurajesh3@gmail.com>
2023-10-21 16:09:21 +0530

    Devtools

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const { app, BrowserWindow } = require('electron');
 
 // the below one is used to check if the app is in production or development
 const isMac = process.platform === 'darwin';
+const isDev = process.env.NODE_ENV !== 'production';
 
 // the below one is used to create a window
 function createMainWindow() {
@@ -11,6 +12,11 @@ function createMainWindow() {
         width: 500,
         height: 600,
     })
+
+    // Open devtools if in dev environment
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 
     // The below one can be used to load a file it can also load a url if we use loadurl() e.x :- twitter.com
     mainWindow.loadFile(path.join(__dirname, 'renderer/index.html'));
@@ -22,6 +28,13 @@ function createMainWindow() {
 // or you can use the below one to create a window when the app is ready it returns a promise
 app.whenReady().then(() => {
     createMainWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    });
+
 });
 
 // the below one is used to quit the app when all the windows are closed except on mac as mac works differently
